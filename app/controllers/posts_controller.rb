2292f12f1d26da442a6e419f191631f53f898138
Jaden Lund <lundjaden@gmail.com>
2022-09-25 18:08:02 -0500

    commit tree 3042482556f742fbff0f2c27a6b9cdde2cfbd553

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,20 @@
 class PostsController < ApplicationController
-
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
+    #send back the error messages if the updated post is invalid
 
     post.update(post_params)
-
-    render json: post
+    if post.valid?
+      render json: post
+    else
+      render json: { errors: post.errors }, status: :unprocessable_entity
+    end
   end
 
   private
@@ -19,5 +22,4 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
-
 end
